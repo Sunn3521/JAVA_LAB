07eaf5b8cfb8436a5abac0c65fc5fbd27fbd96a4
Saahith Dondapati <saahith3521@gmail.com>
2026-03-11 12:20:32 +0530

    Update TSEngineeringAdmission.java

--- Week-4&5/TSEngineeringAdmission.java
+++ Week-4&5/TSEngineeringAdmission.java
@@ -14,18 +14,17 @@ public class TSEngineeringAdmission {
 			int TSEAMCETScore = Integer.parseInt(args[0]);
 			if (TSEAMCETScore < 50){
 				throw new NotEligibleForEngineering("Sorry You Are Not Eligible To Apply Engineering Admission");
-				}
+			}
 			else if (TSEAMCETScore < 70){
 				throw new NotEligibleForVJIT("Sorry You Are Not Eligible To Apply VJIT-Hyderabad");
-				}
-			else 
-				{
+			}
+			else{
 				System.out.println("Welcome to VJIT-Thanks For Your Registration!");
-				}
-			} 
+			}
+		} 
 		catch (NotEligibleForEngineering | NotEligibleForVJIT e){
 			System.out.println("Exception Caught: " + e.getMessage());  
-			}
+		}
         System.out.println("Rest of the Code");  
     }
 }
