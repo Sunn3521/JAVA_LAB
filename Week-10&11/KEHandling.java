81c0ba87bb62678412896835edc590c1f46f19d2
Saahith Dondapati <saahith3521@gmail.com>
2026-04-29 12:13:58 +0530

    Add files via upload

---
+++ Week-10&11/KEHandling.java
@@ -0,0 +1,40 @@
+import java.awt.*;
+import java.awt.event.*;
+class KEHandling extends Frame implements KeyListener
+{
+	String msg="",msg2="";
+	KEHandling(){
+		addKeyListener(this);
+	}
+	public void keyPressed(KeyEvent ke){
+		msg+="Key Pressed";
+		repaint();
+	}
+	public void keyReleased(KeyEvent ke){
+		msg2+=ke.getKeyChar();
+		msg+="Key Released";
+		repaint();
+	}
+	public void keyTyped(KeyEvent ke){
+		msg2+=ke.getKeyChar();
+		msg+="Key Typed";
+		repaint();
+	}
+	public void paint(Graphics g){
+		g.drawString(msg,100,50);
+		g.drawString(msg2,100,100);
+		msg="";
+	}
+	public static void main(String[] args) 
+	{
+		KEHandling kh=new KEHandling();
+		kh.setTitle("My First Window");
+		kh.setSize(500,200);
+		kh.setVisible(true);
+		kh.addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent we){
+				System.exit(0);
+			} 
+		});
+	}
+}
