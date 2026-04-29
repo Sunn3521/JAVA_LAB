1301b0891c60cfb6b5fa148d642fa16c807601e6
Saahith Dondapati <saahith3521@gmail.com>
2026-04-29 12:17:01 +0530

    update

--- Week-10&11/MEHandling.java
+++ Week-10&11/MEHandling.java
@@ -1,21 +1,16 @@
 import java.awt.*;
 import java.awt.event.*;
-
 class MEHandling extends Frame implements MouseListener, MouseMotionListener {
     String msg = "", msg2 = "";
     int x = 150, y = 100;
-
     MEHandling() {
         addMouseListener(this);
         addMouseMotionListener(this);
     }
-
-    // MouseListener methods
     public void mouseClicked(MouseEvent me) {
         msg = "mouse Clicked";
         repaint();
     }
-
     public void mousePressed(MouseEvent me) {
         x = me.getX();
         y = me.getY();
@@ -27,38 +22,30 @@ class MEHandling extends Frame implements MouseListener, MouseMotionListener {
         msg = "mouse Released";
         repaint();
     }
-
     public void mouseEntered(MouseEvent me) {
         msg = "mouse Entered";
         repaint();
     }
-
     public void mouseExited(MouseEvent me) {
         msg = "mouse Exited";
         repaint();
     }
-
-    // MouseMotionListener methods
     public void mouseDragged(MouseEvent me) {
         x = me.getX();
         y = me.getY();
         msg2 = "mouse Dragged";
         repaint();
     }
-
     public void mouseMoved(MouseEvent me) {
         x = me.getX();
         y = me.getY();
         msg2 = "mouse Moved";
         repaint();
     }
-
-    // Paint method
     public void paint(Graphics g) {
         g.drawString(msg, x, y);
-        g.drawString(msg2, x, y + 20); // offset so both are visible
+        g.drawString(msg2, x, y + 20);
     }
-
     public static void main(String args[]) {
         MEHandling mh = new MEHandling();
         mh.setTitle("Mouse Handling Event");
